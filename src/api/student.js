8a7dfcf4087d040db0409fbf6472dfbd86416ddf
fx67ll <2717745014@qq.com>
2021-03-30 17:43:27 +0800

    add new schema model

---
+++ src/api/student.js
@@ -0,0 +1,43 @@
+import request from '@/utils/request'
+
+// 查询学员列表
+export function listStudent(query) {
+	return request({
+		url: '/human/getHumanList',
+		method: 'get',
+		params: query
+	})
+}
+
+// 查询学员详细
+export function getStudent(id) {
+	return request({
+		url: '/human/getHumanById/' + id,
+		method: 'get'
+	})
+}
+// 新增学员
+export function addStudent(data) {
+	return request({
+		url: '/human/createHuman',
+		method: 'post',
+		data: data
+	})
+}
+
+// 修改学员
+export function updateStudent(id, data) {
+	return request({
+		url: '/human/updateHumanById/' + id,
+		method: 'put',
+		data: data
+	})
+}
+
+// 删除学员
+export function delStudent(id) {
+	return request({
+		url: '/human/deleteHumanById/' + id,
+		method: 'delete'
+	})
+}
