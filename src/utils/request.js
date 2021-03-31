ba623a1de7c51251ccce185946cacde747924958
fx67ll <2717745014@qq.com>
2021-03-31 17:18:06 +0800

    update interceptors

--- src/utils/request.js
+++ src/utils/request.js
@@ -53,26 +53,28 @@ service.interceptors.request.use(config => {
 // 响应拦截器
 service.interceptors.response.use(res => {
 		// 未设置状态码则默认成功状态
-		const code = res.data.code || 200;
+		const code = res.data.status || 0;
 		// 获取错误信息
 		const msg = errorCode[code] || res.data.msg || errorCode['default']
-		if (code === 401) {
-			MessageBox.confirm('登录状态已过期，您可以继续留在该页面，或者重新登录', '系统提示', {
-				confirmButtonText: '重新登录',
-				cancelButtonText: '取消',
-				type: 'warning'
-			}).then(() => {
-				store.dispatch('LogOut').then(() => {
-					location.href = '/index';
-				})
-			})
-		} else if (code === 500) {
-			Message({
-				message: msg,
-				type: 'error'
-			})
-			return Promise.reject(new Error(msg))
-		} else if (code !== 200) {
+		// if (code === 401) {
+		// 	MessageBox.confirm('登录状态已过期，您可以继续留在该页面，或者重新登录', '系统提示', {
+		// 		confirmButtonText: '重新登录',
+		// 		cancelButtonText: '取消',
+		// 		type: 'warning'
+		// 	}).then(() => {
+		// 		store.dispatch('LogOut').then(() => {
+		// 			location.href = '/index';
+		// 		})
+		// 	})
+		// } else 
+		// if (code === 500) {
+		// 	Message({
+		// 		message: msg,
+		// 		type: 'error'
+		// 	})
+		// 	return Promise.reject(new Error(msg))
+		// } else 
+		if (code !== 0) {
 			Notification.error({
 				title: msg
 			})
